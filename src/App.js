136589d3a4f5aabc2b99270839ff9ab3ba288585
mryevhenii07 <mrit.works07@gmail.com>
2022-09-12 19:22:16 +0300

    create component Featured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import Best from "./components/Best/Best";
+import Featured from "./components/Featured/Featured";
+
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
       <Best />
+      <Featured />
     </>
   );
 }
--- src/components/Best/Best.jsx
+++ src/components/Best/Best.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import s from "./Best.module.css";
+import Apt1 from "../../assets/apt1.jpeg";
+import Apt2 from "../../assets/apt2.jpeg";
+import Apt3 from "../../assets/apt3.jpeg";
+
 const Best = () => {
-  return <div>Best</div>;
+  return (
+    <div className={s.best}>
+      <h1>Find Best Rated Properties</h1>
+      <div>
+        <p>
+          <span className={s.bold}>All</span>
+        </p>
+        <p>Commercial</p>
+        <p>Residential</p>
+        <p>Agricultural</p>
+      </div>
+      <div className={s.container}>
+        <img src={Apt1} alt="/" />
+        <img src={Apt2} alt="/" />
+        <img src={Apt3} alt="/" />
+      </div>
+      <button className={s.btn}>View All</button>
+    </div>
+  );
 };
 
 export default Best;
