1af27ca2c25c360de2ff7fe24c2672a27cb16b97
mryevhenii07 <mrit.works07@gmail.com>
2022-09-12 18:40:31 +0300

    create component best

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
+import Best from "./components/Best/Best";
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
+      <Best />
     </>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 import s from "./Hero.module.css";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className={s.hero}>
+      <div className={s.content}>
+        <h1>Find the perfect place</h1>
+        <p className={s.searchText}>
+          Search the largest selection of luxury high-rice
+          apartments,multi-family homes, and luxury homes.
+        </p>
+        <form action="" className={s.search}>
+          <div>
+            <input type="text" placeholder="Enter Key" />
+          </div>
+          <div className={s.radio}>
+            <input type="radio" checked />
+            <label htmlFor="">Buy</label>
+            <input type="radio" />
+            <label htmlFor="">Rent</label>
+            <button type="submit">
+              <AiOutlineSearch className={s.icon} />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
